281ee38bc27cb661fcacc6f745a5fb3efa61905c
dayoungim <imdda93@gmail.com>
2023-09-26 15:16:38 +0900

    dy

--- main.js
+++ main.js
@@ -3,4 +3,16 @@ $(function() {
         loop: true,
         slidesPerView: 'auto'
     });
+
+    let idx = 0;
+    $(window).on('scroll', () => {
+        const centerEle = document.elementFromPoint(window.innerWidth/2, window.innerHeight/2);
+        const currentCont = $(centerEle).closest('.container').first();
+        const currentSectionId = currentCont.data('section');
+        
+        if ($(`.list-item[data-section="${currentSectionId}"]`).length) {
+            idx = $(`.list-item[data-section="${currentSectionId}"]`).index() - 1;
+            $('.list .spin').css('top', idx * 40);
+        }
+    });
 });
